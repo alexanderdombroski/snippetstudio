40e8045090f32e2693269c8ef8cf8c0bf01036e8
alexanderdombroski <dom21013@byui.edu>
2025-05-03 16:35:19 -0600

    Feat: Added command to view everyone's snippet gists on Github.

--- src/commands/snippetGists.ts
+++ src/commands/snippetGists.ts
@@ -9,6 +9,10 @@ async function initSnippetGistsCommands(context: vscode.ExtensionContext) {
         vscode.commands.registerCommand("snippetstudio.github.import", async () => {
             await importGist(context);
             vscode.commands.executeCommand("snippetstudio.refreshLocations");
+        }),
+        vscode.commands.registerCommand("snippetstudio.github.browse", async () => {
+            const targetUri = vscode.Uri.parse("https://gist.github.com/search?q=snippetstudio+extension%3A.code-snippets&ref=searchresults");
+            vscode.env.openExternal(targetUri);
         })
     );
 }
