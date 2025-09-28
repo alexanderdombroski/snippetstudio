ff3f22eb15da574ce7e7d974f5be1e9faaaacfa2
alexanderdombroski <dom21013@byui.edu>
2025-09-27 20:56:56 -0600

    feat(test): add importBuiltinExtension success test case

--- src/git/extensionsGithub.test.ts
+++ src/git/extensionsGithub.test.ts
@@ -3,13 +3,19 @@ import * as extensionsGithub from './extensionsGithub';
 import { getOctokitClient } from './octokit';
 import { context } from '../../.vitest/__mocks__/shared';
 import { writeSnippetFile } from '../utils/jsoncFilesIO';
-import { showQuickPick } from '../vscode';
-import { chooseLocalGlobal } from '../utils/user';
+import vscode, { showInformationMessage, showQuickPick } from '../vscode';
+import { chooseLocalGlobal, getFileName } from '../utils/user';
 import type { Octokit } from '@octokit/core' with { 'resolution-mode': 'import' };
 
 const { __fileTextRequest, __folderRequest, importBuiltinExtension } = extensionsGithub;
 
-vi.mock('../utils/jsoncFilesIO');
+vi.mock('../utils/jsoncFilesIO', async () => {
+	const mod = await vi.importActual('../utils/jsoncFilesIO');
+	return {
+		...mod,
+		writeSnippetFile: vi.fn(),
+	};
+});
 vi.mock('../utils/user');
 
 describe.skipIf(!process.env.GITHUB_TOKEN)('extensionsGithub', () => {
@@ -18,19 +24,34 @@ describe.skipIf(!process.env.GITHUB_TOKEN)('extensionsGithub', () => {
 		client = await getOctokitClient(context);
 	});
 	beforeEach(() => {
-		vi.clearAllMocks();
 		vi.resetAllMocks();
+		(vscode.window.withProgress as Mock).mockResolvedValue(['cpp', 'csharp', 'html', 'java']);
 	});
 
-	describe.concurrent('importBuiltinExtension', () => {
-		it("shouldn't save anything if there is no selected file or save location", async () => {
-			(showQuickPick as Mock).mockResolvedValue(undefined);
+	describe.concurrent('importBuiltinExtension--success', () => {
+		it.concurrent('should make requests for all selections', async () => {
+			(showQuickPick as Mock).mockResolvedValue([{ label: 'csharp' }]);
+			(chooseLocalGlobal as Mock).mockResolvedValue('example/path');
+			(getFileName as Mock).mockReturnValue('ms-snippets');
 			await importBuiltinExtension(context);
 
-			expect(writeSnippetFile).not.toBeCalled();
+			expect(showInformationMessage).not.toBeCalled();
+			expect(writeSnippetFile).toBeCalled();
 		});
-		it("shouldn't save if there is no save location", async () => {
-			(showQuickPick as Mock).mockResolvedValue(['python-ms', 'ms-dotnet']);
+	});
+
+	describe('importBuiltinExtension--fail', () => {
+		it.concurrent(
+			"shouldn't save anything if there is no selected file or save location",
+			async () => {
+				(showQuickPick as Mock).mockResolvedValue(undefined);
+				await importBuiltinExtension(context);
+
+				expect(writeSnippetFile).not.toBeCalled();
+			}
+		);
+		it.concurrent("shouldn't save if there is no save location", async () => {
+			(showQuickPick as Mock).mockResolvedValue([{ label: 'python-ms' }, { label: 'ms-dotnet' }]);
 			(chooseLocalGlobal as Mock).mockResolvedValue(undefined);
 			await importBuiltinExtension(context);
 
