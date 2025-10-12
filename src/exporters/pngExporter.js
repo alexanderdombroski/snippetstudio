1fef4d9d0025baeb0d77c2f0fac9bfacef7fdd62
alexanderdombroski <dom21013@byui.edu>
2025-10-12 00:43:02 -0600

    chore: remove unused functions

--- src/exporters/pngExporter.js
+++
@@ -1,7 +0,0 @@
-export async function exportToPNG(canvas) {
-  const dataURL = canvas.toDataURL("image/png");
-  const link = document.createElement("a");
-  link.href = dataURL;
-  link.download = "project.png";
-  link.click();
-}
--- src/exporters/projectSerializer.js
+++
@@ -1,7 +0,0 @@
-export function serializeProject(canvasData) {
-  return JSON.stringify(canvasData, null, 2);
-}
-
-export function importProject(jsonString) {
-  return JSON.parse(jsonString);
-}
