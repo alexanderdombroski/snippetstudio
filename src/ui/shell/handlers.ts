46b971169ed9bdbf6b959831f12d6e9831a14510
alexanderdombroski <dom21013@byui.edu>
2025-10-12 23:30:30 -0600

    feat: add shell snippet command handlers

---
+++ src/ui/shell/handlers.ts
@@ -0,0 +1,16 @@
+import type { ShellTreeItem } from './ShellViewProvider';
+
+/** Command handler to create a new shell snippet */
+export function createShellSnippet() {}
+
+/** Command handler to edit an existing shell snippet */
+// eslint-disable-next-line no-unused-vars
+export function editShellSnippet(item: ShellTreeItem) {}
+
+/** Command handler to delete a shell snippet */
+// eslint-disable-next-line no-unused-vars
+export function deleteShellSnippet(item: ShellTreeItem) {}
+
+/** Command handler to run a shell command snippet */
+// eslint-disable-next-line no-unused-vars
+export function runShellSnippet(item: ShellTreeItem) {}
